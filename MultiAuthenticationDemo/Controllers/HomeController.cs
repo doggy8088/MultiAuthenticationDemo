4228d740182ece7fab1888c8bf8066b2c525cfa9
Will <doggy.huang@gmail.com>
2022-04-05 21:50:10 +0800

    Add project files.

---
+++ MultiAuthenticationDemo/Controllers/HomeController.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiAuthenticationDemo.Models;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace MultiAuthenticationDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly JwtHelpers jwt;
+
+        public HomeController(ILogger<HomeController> logger, JwtHelpers jwt)
+        {
+            _logger = logger;
+            this.jwt = jwt;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult GenerateToken()
+        {
+            return Ok(new
+            {
+                token = jwt.GenerateToken("will")
+            });
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> GetTokenFromJwtBearerAsync()
+        {
+            var accessToken = await HttpContext.GetTokenAsync(JwtBearerDefaults.AuthenticationScheme, "access_token");
+
+            return Ok(new
+            {
+                accessToken
+            });
+        }
+
+        [Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> GetTokenFromOpenIdConnectAsync()
+        {
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectDefaults.AuthenticationScheme, "access_token");
+
+            return Ok(new
+            {
+                accessToken
+            });
+        }
+
+        [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> GetTokenFromCookieAsync()
+        {
+            var accessToken = await HttpContext.GetTokenAsync(CookieAuthenticationDefaults.AuthenticationScheme, "access_token");
+
+            return Ok(new
+            {
+                accessToken
+            });
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public IActionResult GetClaimsFromJwtBearer()
+        {
+            return Ok(new
+            {
+                Claims = HttpContext.User.Claims.Select(c => new
+                {
+                    Type = c.Type,
+                    ShortTypeName = c.Properties.ContainsKey("http://schemas.xmlsoap.org/ws/2005/05/identity/claimproperties/ShortTypeName")
+                        ? c.Properties["http://schemas.xmlsoap.org/ws/2005/05/identity/claimproperties/ShortTypeName"]
+                        : "",
+                    Value = c.Value,
+                    Properties = c.Properties
+                }),
+                ClaimsDetail = HttpContext.User.Claims.Select(c => new
+                {
+                    Subject = new
+                    {
+                        c.Subject?.Name,
+                        c.Subject?.NameClaimType,
+                        c.Subject?.RoleClaimType,
+                        c.Subject?.Label,
+                        c.Subject?.Actor,
+                        c.Subject?.AuthenticationType,
+                        c.Subject?.BootstrapContext,
+                    },
+                    c.Type,
+                    c.ValueType,
+                    c.Value,
+                    c.Issuer,
+                    c.OriginalIssuer,
+                    Properties = c.Properties.Select(p => new
+                    {
+                        p.Key,
+                        p.Value
+                    })
+                })
+            });
+        }
+
+        [Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)]
+        public IActionResult GetClaimsFromOpenIdConnect()
+        {
+            return Ok(new
+            {
+                Claims = HttpContext.User.Claims.Select(c => new
+                {
+                    Type = c.Type,
+                    ShortTypeName = c.Properties.ContainsKey("http://schemas.xmlsoap.org/ws/2005/05/identity/claimproperties/ShortTypeName")
+                        ? c.Properties["http://schemas.xmlsoap.org/ws/2005/05/identity/claimproperties/ShortTypeName"]
+                        : "",
+                    Value = c.Value,
+                    Properties = c.Properties
+                }),
+                ClaimsDetail = HttpContext.User.Claims.Select(c => new
+                {
+                    Subject = new
+                    {
+                        c.Subject?.Name,
+                        c.Subject?.NameClaimType,
+                        c.Subject?.RoleClaimType,
+                        c.Subject?.Label,
+                        c.Subject?.Actor,
+                        c.Subject?.AuthenticationType,
+                        c.Subject?.BootstrapContext,
+                    },
+                    c.Type,
+                    c.ValueType,
+                    c.Value,
+                    c.Issuer,
+                    c.OriginalIssuer,
+                    Properties = c.Properties.Select(p => new
+                    {
+                        p.Key,
+                        p.Value
+                    })
+                })
+            });
+        }
+
+        [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+        public IActionResult GetClaimsFromCookie()
+        {
+            return Ok(new
+            {
+                Claims = HttpContext.User.Claims.Select(c => new
+                {
+                    Type = c.Type,
+                    ShortTypeName = c.Properties.ContainsKey("http://schemas.xmlsoap.org/ws/2005/05/identity/claimproperties/ShortTypeName")
+                        ? c.Properties["http://schemas.xmlsoap.org/ws/2005/05/identity/claimproperties/ShortTypeName"]
+                        : "",
+                    Value = c.Value,
+                    Properties = c.Properties
+                }),
+                ClaimsDetail = HttpContext.User.Claims.Select(c => new
+                {
+                    Subject = new
+                    {
+                        c.Subject?.Name,
+                        c.Subject?.NameClaimType,
+                        c.Subject?.RoleClaimType,
+                        c.Subject?.Label,
+                        c.Subject?.Actor,
+                        c.Subject?.AuthenticationType,
+                        c.Subject?.BootstrapContext,
+                    },
+                    c.Type,
+                    c.ValueType,
+                    c.Value,
+                    c.Issuer,
+                    c.OriginalIssuer,
+                    Properties = c.Properties.Select(p => new
+                    {
+                        p.Key,
+                        p.Value
+                    })
+                })
+            });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MultiAuthenticationDemo/Program.cs
@@ -0,0 +1,88 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<JwtHelpers>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+
+    })
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // 當驗證失敗時，回應標頭會包含 WWW-Authenticate 標頭，這裡會顯示失敗的詳細錯誤原因
+        options.IncludeErrorDetails = true; // 預設值為 true，有時會特別關閉
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // 透過這項宣告，就可以從 "sub" 取值並設定給 User.Identity.Name
+            NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+            // 透過這項宣告，就可以從 "roles" 取值，並可讓 [Authorize] 判斷角色
+            RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+
+            // 一般我們都會驗證 Issuer
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("JwtSettings:Issuer"),
+
+            // 通常不太需要驗證 Audience
+            ValidateAudience = false,
+            //ValidAudience = "JwtAuthDemo", // 不驗證就不需要填寫
+
+            // 一般我們都會驗證 Token 的有效期間
+            ValidateLifetime = true,
+
+            // 如果 Token 中包含 key 才需要驗證，一般都只有簽章而已
+            ValidateIssuerSigningKey = false,
+
+            // "1234567890123456" 應該從 IConfiguration 取得
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JwtSettings:SignKey")))
+        };
+    })
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+    {
+        options.Authority = "https://accounts.google.com";
+
+        options.ClientId = builder.Configuration["GoogleOAuth:client_id"];
+        options.ClientSecret = builder.Configuration["GoogleOAuth:client_secret"];
+        options.ResponseType = "code id_token";
+
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+
+        // 拿到 Token 之後可以存入 Cookies 保存
+        options.SaveTokens = true;
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
